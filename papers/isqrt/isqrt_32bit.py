03bd029258c9e8eafc014b360e4a0157fb85219d
removedporn <86824510+removedporn@users.noreply.github.com>
2022-08-13 19:34:57 +0800

    Delete papers/isqrt directory

--- papers/isqrt/isqrt_32bit.py
+++
@@ -1,9 +0,0 @@
-def isqrt(n):
-    e = (32 - n.bit_length()) // 2
-    m = n << 2*e
-    a = 1 + (m >> 30)
-    a = (a << 1) + (m >> 27) // a
-    a = (a << 3) + (m >> 21) // a
-    a = (a << 7) + (m >> 9) // a
-    a = a >> e
-    return a if a*a <= n else a - 1
